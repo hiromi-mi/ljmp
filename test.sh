bb5ef6ece7d4dd7d57979ff0c078119d8765ca49
hiromi-mi <hiromi-mi@cat.zaq.jp>
2018-09-03 19:58:51 +0900

    Add initial start test.

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# tmux is required.
+
+# Start Session
+tmux -u new-session -d -x 90 -y 10 -t ljmp
+
+# Start ljmp
+tmux send -t ljmp "./ljmp ljmp.c" ENTER
+sleep 0.1
+tmux capture-pane -p
+# Capture With Colour
+tmux capture-pane -p -e
+
+# Getting Cursor Position
+tmux display -p '#{cursor_x}, #{cursor_y}'
+
+# Quit ljmp
+tmux send -t ljmp C-q C-q C-q
+tmux -u kill-session -t ljmp
+
