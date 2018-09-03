5ec28bfdd869ad821a897cf50734b136f645354b
hiromi-mi <hiromi-mi@cat.zaq.jp>
2018-09-03 21:30:08 +0900

    add test1

--- test.sh
+++ test.sh
@@ -5,15 +5,18 @@
 # Start Session
 tmux -u new-session -d -x 90 -y 10 -t ljmp
 
+bash ./test1.sh
+
 # Start ljmp
 tmux send -t ljmp "./ljmp ljmp.c" ENTER
 sleep 0.1
 tmux capture-pane -p
 # Capture With Colour
-tmux capture-pane -p -e
+#tmux capture-pane -p -e
 
 # Getting Cursor Position
-tmux display -p '#{cursor_x}, #{cursor_y}'
+#tmux display -p '#{cursor_x}'
+#tmux display -p '#{cursor_y}'
 
 # Quit ljmp
 tmux send -t ljmp C-q C-q C-q
---
+++ test1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Testsuite no.1 ライセンス表記の確認
+
+# Start ljmp
+tmux send -t ljmp "./ljmp ljmp.c" ENTER
+sleep 0.1
+tmux capture-pane -p | head -n 8 | diff -y test1.ok -
+if [ $? -ne 0 ]
+then
+   # 変化があり
+   echo "Test1 Failed."
+fi
+tmux send -t ljmp C-q C-q C-q
